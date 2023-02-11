12cbedd15004fcae3e402af3c12b69df47e14900
Misa2503 <alfapomoc234@gmail.com>
2023-02-11 19:13:31 +0100

    register javascript check errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,47 @@
-var box = document.querySelector('.box')
-// window.addEventListener('load', function(){
-//     console.log(box)
+var registerBtn = document.querySelector('.register')
+var errorMessage = document.querySelector('.error-message')
+
+ // registerBtn.addEventListener('click', function(e) {
+//      e.preventDefault();
+
+//      var formInputs = document.querySelectorAll('.input')
+//     for(var i=0; i < formInputs.length; i++) {
+//          if(formInputs[i].value == ''){
+//              errorMessage.innerHTML = 'Error. Input is empty'
+//          }
+//      }
 // })
 
-var hideBtn = document.querySelector('#hide-div-btn')
-var showBtn = document.querySelector('#show-div-btn')
+var usernameInput = document.querySelector('.input_username')
+var emailInput = document.querySelector('.input_email')
+var passwordInput = document.querySelector('.input_password')
 
 
-hideBtn.addEventListener('click', function() {
-    box.style.display = "none"
-    
+
+registerBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if(usernameInput.value == ''){
+        document.querySelector('.error-message-username').innerHTML = 'Error. Input is empty.'
+    }
+    else{
+        document.querySelector('.error-message-username').innerHTML = ''
+    }
+        if (emailInput.value == ''){
+            document.querySelector('.error-message-email').innerHTML = 'Error. Input is empty.'
+        }
+        else{
+            document.querySelector('.error-message-email').innerHTML = ''
+        }
+     if (passwordInput.value == ''){
+            document.querySelector('.error-message-password').innerHTML = 'Error. Input is empty.'
+        }
+        else{
+            document.querySelector('.error-message-password').innerHTML = ''
+        }
+
+        if(errors == 0) {
+            location.href = 'register.php'
+        }
+        
 })
-
-showBtn.addEventListener('click', function(){
-    box.style.display = "inline-block"
-})
-
-
--- templates/test.php
+++ templates/test.php
@@ -4,12 +4,16 @@
 get_header();
 
 ?>
-<form action="" method="post">
-    <input type="text" name="username" class="input input_username" placeholder="Username"><br>
-    <input type="email" name="email" class="input input_email" placeholder="Email"><br>
-    <input type="password" name="password" class="input input_password" placeholder="Password"><br>
+<form action="register.php" method="post">
+    <input type="text" name="username" class="input input_username" placeholder="Username">
+    <p class="error-message-username"></p><br>
+    <input type="email" name="email" class="input input_email" placeholder="Email">
+    <p class="error-message-email"></p><br>
+    <input type="password" name="password" class="input input_password" placeholder="Password">
+    <p class="error-message-password"></p><br>
 
-    <input type="submit" class="register" value="Register">
+    <input type="submit" class="register" value="Register"><br>
+    <p class="error-message"></p>
 </form>
 
 <?php get_footer(); ?>
